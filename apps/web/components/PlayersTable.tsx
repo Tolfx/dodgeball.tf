fe7f610f0d94061c8dec4e2f81cb8365ffb5c69e
Tolfx <matteus@tolfx.dev>
2022-11-02 16:59:49 +0100

    feat: small changes

--- apps/web/components/PlayersTable.tsx
+++ apps/web/components/PlayersTable.tsx
@@ -36,7 +36,7 @@ export default function PlayersTable({ players }: Props)
   // Table of players
   return (
     <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+      <Table sx={{ minWidth: 650 }}>
         <TableHead>
           <TableRow>
             <StyledTableCell>Name</StyledTableCell>
