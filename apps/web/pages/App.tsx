0f1cad7c0a472ec44fa769ef7e892a92303c421d
Matteus Westman <matteus.westman@surikat.se>
2022-11-02 10:46:44 +0100

    feat: added favicon

--- apps/web/pages/App.tsx
+++ apps/web/pages/App.tsx
@@ -14,6 +14,12 @@ export default function App()
         <meta name="keywords" content="dodgeball.tf,dodgeball,tf2,team fortress 2, tfdb" />
         <meta name="author" content="Tolfx" />
         {/* Meta data about color */}
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#8650AE" />
       </Head>
       <Container>
