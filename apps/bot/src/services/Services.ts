f4e01ef8e222bdc1c7e5f2b95bbe5e44c8e1cb99
Tolfx <matteus@tolfx.dev>
2022-12-10 17:17:23 +0100

    feat: modified services

--- apps/bot/src/services/Services.ts
+++ apps/bot/src/services/Services.ts
@@ -1,4 +1,6 @@
+import { MongoDb } from "@dodgeball/mongodb";
 import { Client } from "discord.js";
+import { Application } from "express";
 import mysql from 'mysql';
 import CacheService from "./CacheService";
 
@@ -6,12 +8,16 @@ export default class Services
 {
   public discordClient: Client;
   private mysqlConnection: mysql.Connection
+  private mongodb: MongoDb;
+  private server: Application;
   private cacheService?: CacheService;
 
-  constructor(discordClient: Client, mysqlConnection: mysql.Connection)
+  constructor(discordClient: Client, mysqlConnection: mysql.Connection, mongodb: MongoDb, server: Application)
   {
     this.discordClient = discordClient;
     this.mysqlConnection = mysqlConnection;
+    this.mongodb = mongodb;
+    this.server = server;
   }
 
   getDiscordClient()
@@ -22,6 +28,16 @@ export default class Services
   getMysqlConnection()
   {
     return this.mysqlConnection;
+  }
+
+  getMongoDB()
+  {
+    return this.mongodb;
+  }
+
+  getServer()
+  {
+    return this.server;
   }
 
   setCaches(cacheService: CacheService)
