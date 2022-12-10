b2c91c8f9a9ea0eb90895a481df750f6f8e8c4cc
Tolfx <matteus@tolfx.dev>
2022-12-10 17:16:51 +0100

    feat: modified queries

--- apps/bot/src/mysql/queries/GetTopSpeedPlayers.ts
+++ apps/bot/src/mysql/queries/GetTopSpeedPlayers.ts
@@ -8,7 +8,7 @@ export type TopSpeedPlayer = {
   serverid: string;
 }
 
-export default function GetTopSpeedPlayers(serverid: string, steamid?: string)
+export default function GetTopSpeedPlayers(serverid?: string, steamid?: string)
 {
   // we want to select database `topspeed` and table `topspeed`
   // We should also select all columns but where serverid is equal to the serverid
@@ -17,7 +17,10 @@ export default function GetTopSpeedPlayers(serverid: string, steamid?: string)
 
   // If steamid is provided, we want to select where steamid is equal to the steamid
 
-  const query = `SELECT * FROM topspeed.topspeed WHERE serverid = '${serverid}' ${steamid ? ` AND steamid = '${steamid}'` : ''} ORDER BY topspeed DESC`;
+  const serveridQuery = serverid ? `${!steamid ? 'WHERE ' : ''}serverid = '${serverid}'` : '';
+  const steamidQuery = steamid ? `${serverid ? 'AND ' : 'WHERE '}steamid = '${steamid}'` : '';
+
+  const query = `SELECT * FROM topspeed.topspeed ${serveridQuery} ${steamidQuery} ORDER BY topspeed DESC`;
 
   return (connection: mysql.Connection): Promise<Array<TopSpeedPlayer>> => new Promise((resolve, reject) =>
   {
