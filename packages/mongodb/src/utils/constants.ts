962e9777e114cc11a027c7140224dbec39a96dff
Tolfx <matteus@tolfx.dev>
2022-12-10 17:15:56 +0100

    feat: small changes

---
+++ packages/mongodb/src/utils/constants.ts
@@ -0,0 +1,5 @@
+export const MONGO_HOST = process.env.MONGO_HOST || 'localhost';
+export const MONGO_PORT = parseInt(process.env.MONGO_PORT || '27017');
+export const MONGO_DATABASE = process.env.MONGO_DATABASE || 'dodgeball';
+export const MONGO_USERNAME = process.env.MONGO_USERNAME || 'root';
+export const MONGO_PASSWORD = process.env.MONGO_PASSWORD || 'root';
