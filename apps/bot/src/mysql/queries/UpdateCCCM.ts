ce3cef2f94a4f2f17a8a081649fb8a7ed01718c3
Tolfx <matteus@tolfx.dev>
2022-12-20 21:06:03 +0100

    feat: updated query

--- apps/bot/src/mysql/queries/UpdateCCCM.ts
+++ apps/bot/src/mysql/queries/UpdateCCCM.ts
@@ -20,8 +20,12 @@ export default function UpdateCCCM(options: UpdateCCCMOptions)
   // hidetag is 0
   // We have different colors and stuff for different donator levels
 
-  // Just ensure not to update data that is null, aka don't add it in the query
-  const queryUpdateCCCM = `UPDATE cccm.cccm_users SET ${options.tag ? `tag = '${options.tag}', ` : ''}${options.chatcolor ? `chatcolor = '${options.chatcolor}', ` : ''}${options.namecolor ? `namecolor = '${options.namecolor}', ` : ''}${options.tagcolor ? `tagcolor = '${options.tagcolor}' ` : ''} WHERE auth = '${(new SteamID(options.steamid).steam2())}';`;
+  // Just ensure not to update data that is null, aka don't add it in the query, we also need to ensure that we don't add a comma at the end of the query if we don't have any data to update
+
+  let queryUpdateCCCM = `UPDATE cccm.cccm_users SET ${options.tag ? `tag = '${options.tag}', ` : ''}${options.chatcolor ? `chatcolor = '${options.chatcolor}', ` : ''}${options.namecolor ? `namecolor = '${options.namecolor}', ` : ''}${options.tagcolor ? `tagcolor = '${options.tagcolor}', ` : ''}WHERE auth = '${(new SteamID(options.steamid).steam2())}';`;
+
+  // Clean it by removing the last comma and space where it is ", WHERE auth"
+  queryUpdateCCCM = queryUpdateCCCM.replace(/, WHERE auth/g, ' WHERE auth');
 
   return (connection: mysql.Connection): Promise<Array<any>> => new Promise((resolve, reject) =>
   {
