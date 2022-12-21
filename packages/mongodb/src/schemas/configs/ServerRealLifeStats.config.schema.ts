2e41bf78e4e2f9ca1f630be3c162183a943c4436
Matteus Westman <matteus.westman@surikat.se>
2022-12-21 14:32:46 +0100

    feat: updated mongodb

---
+++ packages/mongodb/src/schemas/configs/ServerRealLifeStats.config.schema.ts
@@ -0,0 +1,18 @@
+import { model, Schema, Document } from "mongoose";
+
+export interface ServerRealLifeStats
+{
+  serverId: string;
+  discordChannelId: string;
+  discordGuildId: string;
+  discordMessageId: string;
+}
+
+export const ServerRealLifeStatsSchema: Schema = new Schema({
+  serverId: { type: String, required: true },
+  discordChannelId: { type: String, required: true },
+  discordGuildId: { type: String, required: true },
+  discordMessageId: { type: String, required: true }
+});
+
+export const ServerRealLifeStatsModel = model<ServerRealLifeStats & Document>("configs", ServerRealLifeStatsSchema);
--- packages/mongodb/src/schemas/index.ts
+++ packages/mongodb/src/schemas/index.ts
@@ -1,3 +1,4 @@
 export * from "./Server.schema";
 export * from "./LinkedAccont.schema";
 export * from "./DonatorUser.schema";
+export * from "./configs"
