0673689d9730d651899a4349ebf47065cf01b3b0
Matteus Westman <matteus.westman@surikat.se>
2022-12-21 13:27:04 +0100

    feat: modified get admins
    Possible to add optional parameter to include donators too

--- apps/bot/src/mysql/queries/GetAdmins.ts
+++ apps/bot/src/mysql/queries/GetAdmins.ts
@@ -19,10 +19,10 @@ export type AdminSourcebans = {
   lastvisit: number;
 }
 
-export default function GetAdmins()
+export default function GetAdmins(includeDonators = false)
 {
   // We don't want to query players that has "Donator |"" in their name
-  const query = `SELECT * FROM sourcebans.sb_admins WHERE user NOT LIKE '%Donator |%'`;
+  const query = `SELECT * FROM sourcebans.sb_admins${includeDonators ? '' : ` WHERE user NOT LIKE '%Donator |%'`}`;
 
   return (connection: mysql.Connection): Promise<Array<AdminSourcebans>> => new Promise((resolve, reject) =>
   {
