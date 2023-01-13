ea3d0802debfd9b416b018bda0045e40420e75be
Tolfx <matteus@tolfx.dev>
2023-01-13 10:00:36 +0100

    feat: modified async await

--- apps/bot/src/util/AsyncAwait.ts
+++ apps/bot/src/util/AsyncAwait.ts
@@ -3,8 +3,17 @@
  * @param method {Promise<T>} The method to await
  * @returns [data, error]
  */
-export default async function AsyncAwait<T>(method: Promise<T>) {
-  return method
-    .then((data: T) => [data, null])
-    .catch((err: Error) => [null, err]);
+export default async function AsyncAwait<T>(
+  method: Promise<T>
+): Promise<[T | null, null | Error]> {
+  try {
+    const data = await method;
+    return [data, null];
+  } catch (error) {
+    const isError = error instanceof Error;
+    if (!isError) {
+      return [null, new Error(String(error))];
+    }
+    return [null, error];
+  }
 }
